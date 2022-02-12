defb7fa6e532c8b770aea21cfddfe12cecd513a2
Nicolas Vitry <74904811+Nv974@users.noreply.github.com>
2022-02-12 13:10:52 +0400

    first

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    Vue App
+<!-- 3 <test></test>-->
+  </div>
+</template>
+
+<script>
+// 1 import Test from './components/Test/Test'
+console.log( '%c' + 'test', 'color:#f0f ; font-size: 1.3rem' );
+
+
+export default {
+  name: 'App',
+  components: {
+    // 2 Test
+  }
+}
+</script>
+
+<style lang="scss" >
+@import "src/assets/scss/style";
+</style>
